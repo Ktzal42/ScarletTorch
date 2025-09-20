ade3f61ea040b515f91564b9864e8fbf5d001813
Aden-C137 <adenekicioglu@gmail.com>
2025-09-21 00:35:06 +0300

    letitlight....

--- player.gd
+++ player.gd
@@ -12,18 +12,19 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if is_on_floor():
-		velocity.y = JUMP1_VELOCITY
-		
-	elif Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction and is_on_floor():
 		velocity.x = direction * SPEED
+		velocity.y = JUMP1_VELOCITY
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, 0)
+		
 
 	move_and_slide()
