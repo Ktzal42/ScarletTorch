2d56cc22fe63f3c311ba2422ae6ecb7483f0aa98
Aden-C137 <adenekicioglu@gmail.com>
2025-09-21 00:14:28 +0300

    letitlight...

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const JUMP1_VELOCITY = -100
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +12,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if is_on_floor():
+		velocity.y = JUMP1_VELOCITY
+		
+	elif Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
