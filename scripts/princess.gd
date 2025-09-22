48641175941695ef65dc4252f67176d2dcee630d
Ktzal42 <adenekicioglu@gmail.com>
2025-09-23 00:22:09 +0300

    win loose
    yeah

--- scripts/princess.gd
+++ scripts/princess.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
 @onready var game_manager = %GameManager
-@onready var winsound_2: AudioStreamPlayer2D = $winsound2
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $"../AudioStreamPlayer2D"
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if game_manager.score == 5:
 		get_tree().change_scene_to_file("res://scenes/winscreen.tscn")
-		
 	else: 
 		get_tree().change_scene_to_file("res://scenes/lose_screen.tscn")
 			
--- scripts/winscreen.gd
+++ scripts/winscreen.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $Player/AudioStreamPlayer2D
 
 func _on_button_pressed() -> void:
-		get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
-	
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
+
+func _on_winmusic_body_entered(body: Node2D) -> void:
+	audio_stream_player_2d.play()
